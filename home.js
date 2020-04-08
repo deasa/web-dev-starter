7ad0146e17d5d934ef5d9d81f45f72a40f08095a
Brendan Ashton <brendan.ashton@biofiredx.com>
2020-04-07 23:05:01 -0600

    finished demo

--- home.js
+++ home.js
@@ -1,8 +1,28 @@
 
-let price = 49.99
-    name = 'Hiking Boots',
-    discounted = false;
+let person = {
+    name: 'John',
+    age: 32,
+    partTime: false,
+    showInfo: function(realAge){
+        showMessage(this.name + " is " + realAge)
+    }
+}
 
-showMessage(name);
+person.showInfo(34); 
 
 changePercentOff(99);
+
+const reviewBtn = document.getElementById('review-button');
+
+reviewBtn.addEventListener('click', function(){
+    const reviewBox = document.getElementById('review');
+
+    if (reviewBox.classList.contains('d-none')){
+        reviewBox.classList.remove('d-none')
+        reviewBtn.textContent = "HIDE REVIEW";
+    }
+    else {
+        reviewBox.classList.add('d-none');
+        reviewBtn.textContent = "SEE REVIEW";
+    }
+})
